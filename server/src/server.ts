269bad1408b9580ad992451bffc8965ae6db3b69
davidbuzatu-marian <davidm.buzatu@gmail.com>
2024-04-11 16:11:38 +0300

    Startup of lsp done

---
+++ server/src/server.ts
@@ -0,0 +1,65 @@
+import log from "./log";
+import { initialize } from "./methods/initialize";
+
+interface Message {
+  jsonrpc: string;
+}
+
+export interface RequestMessage extends Message {
+  id: number | string;
+  method: string;
+  params?: unknown[] | object;
+}
+
+type RequestMethod = (message: RequestMessage) => unknown;
+
+const methodLookup: Record<string, RequestMethod> = {
+  initialize,
+};
+
+const respond = (id: RequestMessage["id"], result: unknown) => {
+  const message = JSON.stringify({ id, result });
+  const messageLength = Buffer.byteLength(message, "utf-8");
+  const header = `Content-Length: ${messageLength}\r\n\r\n`;
+
+  log.write(header + message);
+  process.stdout.write(header + message);
+};
+
+let buffer = "";
+// This is just a chunk. It may be the beginning of a message, the end of it, or some other bit. We therefore need to buffer this data.
+process.stdin.on("data", (chunk) => {
+  buffer += chunk;
+
+  while (true) {
+    // Check for the Content-Length header
+    const lengthMatch = buffer.match(/Content-Length: (\d+)\r\n/);
+    if (!lengthMatch) {
+      break;
+    }
+
+    const contentLength = parseInt(lengthMatch[1], 10);
+    const messageStart = buffer.indexOf("\r\n\r\n") + 4;
+
+    // Continue unless full message is in the buffer
+    if (buffer.length < messageStart + contentLength) {
+      break;
+    }
+    const rawMessage = buffer.slice(messageStart, messageStart + contentLength);
+    const jsonMessage = JSON.parse(rawMessage);
+
+    log.write({
+      id: jsonMessage.id,
+      method: jsonMessage.method,
+    });
+
+    // Handle the method
+    const method = methodLookup[jsonMessage.method];
+    if (method) {
+      respond(jsonMessage.id, method(jsonMessage));
+    }
+
+    // Remove the processed message from buffer
+    buffer = buffer.slice(messageStart + contentLength);
+  }
+});
